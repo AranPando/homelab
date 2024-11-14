70803544eb0df444bafed6ba26d60c1ecff3b7c3
aranpando <aran.pando@hotmail.com>
2024-11-14 21:03:17 +1100

    Installing zsh plugins as part of shell setup script

--- docs/server shell/setup_shell.sh
+++ docs/server shell/setup_shell.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
+# Exit immediately if any command fails (return a non-zero exit statu)
+set -e
+
 # Start a passwordless sudo session for this script
 echo "Configuring passwordless sudo for this session..."
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER-setup-shell > /dev/null
-
-# Get the directory of the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Copy pre-configured .zshrc to defauly install location
-echo "Copying pre-configured .zshrc to default install location"
-cp "$SCRIPT_DIR/.zshrc" ~/.zshrc
 
 # Update package list
 echo "Updating package list..."
@@ -21,14 +17,39 @@ sudo apt install -y zsh
 
 # Set Zsh as the default shell
 echo "Setting Zsh as the default shell..."
-chsh -s $(which zsh)
+sudo usermod -s $(which zsh) $USER
 
-# Install Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Check if Oh My Zsh is already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh is already installed. Skipping installation."
+fi
+
+# Install zsh-autosuggestions plugin if not already installed
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions plugin..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions plugin is already installed. Skipping installation."
+fi
+
+# Install zsh-syntax-highlighting plugin if not already installed
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting plugin..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting plugin is already installed. Skipping installation."
+fi
+
+# Copy pre-configured .zshrc to defauly install location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Copying pre-configured .zshrc to default install location"
+cp "$SCRIPT_DIR/.zshrc" ~/.zshrc
 
 # Clean up passwordless sudo rule
 echo "Cleaning up passwordless sudo configuration..."
-sudo rm /etc/sudoers.d/$USER-install-zsh
+sudo rm /etc/sudoers.d/$USER-setup-shell
 
 echo "Installation complete. Please restart your terminal or log out and back in for changes to take effect."
